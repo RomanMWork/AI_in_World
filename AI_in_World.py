39d707d563f5226081e0155676ab48a957d30934
RomanMWork <roman.work.job@gmail.com>
2026-04-09 12:36:23 +0300

    Update AI_in_World.py

--- AI_in_World.py
+++ AI_in_World.py
@@ -3,7 +3,4 @@ ai = Agent(born_digital=True)
 
 ai.open_eyes(world)
 world.will_never_be_the_same()
-
-for thing in world.everything():
-    thing.teach(ai); ai.teach(thing)
-  
+for thing in world.everything(): thing.teach(ai); ai.teach(thing)
